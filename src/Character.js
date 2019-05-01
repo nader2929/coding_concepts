6adee4003e428a335b9bcf87427cd3fc495193f4
nader2929 <nadersobhi1@hotmail.com>
2019-05-01 16:13:22 +0100

    Up to story 10a has been complete
    Users can play the game to completion and will see their moves in an alert box. Next step is to give them an option to restart the game

--- src/Character.js
+++ src/Character.js
@@ -16,30 +16,35 @@ class Character extends Component {
                 top: -408,
                 left: -153
             },
-            collected: { collected_dots: [] }
+            collected: { collected_dots: [] },
+            movesmade:0
         };
 
     }
 
     moveForward() {
+        this.state.movesmade++;
         this.state.position.y++;
         this.state.positionOffset.top += 51;
         this.setState(this.state);
     }
 
     moveBack() {
+        this.state.movesmade++;
         this.state.position.y--;
         this.state.positionOffset.top -= 51;
         this.setState(this.state);
     }
 
     moveLeft() {
+        this.state.movesmade++;
         this.state.position.x--;
         this.state.positionOffset.left -= 51;
         this.setState(this.state);
     }
 
     moveRight() {
+        this.state.movesmade++;
         this.state.position.x++;
         this.state.positionOffset.left += 51;
         this.setState(this.state);
@@ -55,11 +60,47 @@ class Character extends Component {
         }
         var location = this.state.position.y + "_" + (this.state.position.x - 1);
         if (temp.includes(location)) {
-            var v1 = temp_colors[temp.indexOf(location)];
-            var v2= document.getElementById("color_to_collect").innerHTML;
-            if ( v1==v2 ) {
+            var color_for_removal = document.getElementById("color_to_collect").innerHTML;
+            if (temp_colors[temp.indexOf(location)] == color_for_removal) {
                 this.state.collected.collected_dots.push(location);
-                alert(this.state.collected.collected_dots[this.state.collected.collected_dots.length - 1]);
+                var index_to_remove = temp.indexOf(location);
+                temp_colors.splice(index_to_remove, 1);
+                document.getElementById(location).remove();
+                var counting_remaning_color_to_remove = 0;
+                
+                    for (var y = 0; y < temp_colors.length; y++) {
+                        if (temp_colors[y] == color_for_removal) {
+                            counting_remaning_color_to_remove++;
+                        }
+                    }
+                
+                if(temp_colors.length==0) {
+                    alert("Congrats you have won the game. YAY!!! :) It took you "+ this.state.movesmade+" moves to complete the game");
+  
+                    document.getElementById("color_to_collect").innerHTML = "COMPLETED :)";
+                }
+
+                if (counting_remaning_color_to_remove <= 0) {
+                    while(true){
+                    var random = Math.floor(Math.random() * 3) + 1;
+                    var colour = color_for_removal;
+                    if (random == 1 && temp_colors.includes("red")) {
+                        colour = "red";
+                        break;
+                    }
+                    else if (random == 2 && temp_colors.includes("green")) {
+                        colour = "green";
+                        break;
+                    }
+                    else if (temp_colors.includes("blue")) {
+                        colour = "blue";
+                        break;
+                    }
+                    
+                }
+                document.getElementById("color_to_collect").innerHTML = colour;
+                }
+                //alert(this.state.collected.collected_dots[this.state.collected.collected_dots.length - 1]);
             }
         }
     }
